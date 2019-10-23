c5a5215e17149ce838af009639258cd14d4b5131
Carl Larson <cslarson@gmail.com>
2019-10-23 14:36:49 -0400

    revert to use local port 8545

--- truffle.js
+++ truffle.js
@@ -45,12 +45,12 @@ module.exports = {
   networks: {
     development: {
       host: 'localhost',
-      port: 8546,
+      port: 8545,
       network_id: '*',
     },
     docker: {
       host: 'devchain',
-      port: 8546,
+      port: 8545,
       network_id: '*'
     },
     mainnet: {
